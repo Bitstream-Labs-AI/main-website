c3913a243cc91a47ec9dab0e52c6a444765207d1
Betsalel Williamson <saul.williamson@ieee.org>
2025-12-30 15:02:05 -0800

    add code comment

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,6 +1,7 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
+// Not yet used, but useful for global state. Will be used to help with adding download counter views
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
   const doubleCount = computed(() => count.value * 2)
